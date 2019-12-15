4d476409aa380571a5b505a3d966b0f3506c4f71
abhilashbn93 <39217044+abhilashbn93@users.noreply.github.com>
2019-12-15 18:49:13 +0530

    Update config.php

--- website_defect/config.php
+++ website_defect/config.php
@@ -5,21 +5,7 @@
  *
  * @var string
  */
-function config($key = '')
-{
-    $config = [
-        'name' => 'Simple PHP Website',
-        'nav_menu' => [
-            '' => 'Home',
-            'content/about-us.php' => 'About Us',
-            'content/products.php' => 'Products',
-            'content/contact.php' => 'Contact',
-        ],
-        'template_path' => 'template',
-        'content_path' => 'content',
-        'pretty_uri' => true,
-        'version' => 'v2.0',
+function c        'version' => 'v2.0',
     ];
 
     return isset($config[$key]) ? $config[$key] : null;
-}
